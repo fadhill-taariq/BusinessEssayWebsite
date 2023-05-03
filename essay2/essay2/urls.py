1d6fd313e4e16e1f368a16abb9181f2f5bc68c58
Taariq <94060158+fadhill-taariq@users.noreply.github.com>
2023-05-03 18:59:50 +0100

    Add files via upload

--- essay2/essay2/urls.py
+++ essay2/essay2/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from homepage.views import homepage
+from homepage import views
 
 urlpatterns = [
     path('', include('homepage.urls')),
--- essay2/homepage/views.py
+++ essay2/homepage/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+def homepage(request):
+    return render(request, 'home.html')
 
 # Create your views here.
